16e48aacdca45e4730930fe380b255d611f2ace5
tianwamg <987897856@qq.com>
2020-07-11 16:09:25 +0800

    initial

---
+++ gatewap-predicate/src/main/java/com/predicate/gatewappredicate/GatewapPredicateApplication.java
@@ -0,0 +1,13 @@
+package com.predicate.gatewappredicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewapPredicateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewapPredicateApplication.class, args);
+    }
+
+}
---
+++ gatewap-predicate/src/test/java/com/predicate/gatewappredicate/GatewapPredicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.predicate.gatewappredicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewapPredicateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
