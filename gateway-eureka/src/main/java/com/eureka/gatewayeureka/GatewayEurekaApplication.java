2d1b90d16a1ea0761e0168970dd09c5f9279da81
tianwamg <987897856@qq.com>
2020-07-12 22:34:21 +0800

    gateway-eureka

---
+++ gateway-eureka/src/main/java/com/eureka/gatewayeureka/GatewayEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.eureka.gatewayeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayEurekaApplication.class, args);
+    }
+
+}
