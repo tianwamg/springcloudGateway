d573159d7b7a4179b42bb42dc9ee1577394b3837
tianwamg <987897856@qq.com>
2020-07-12 22:08:58 +0800

    filter

---
+++ gateway-filter/src/main/java/config/GatewayConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import filter.TokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+}
